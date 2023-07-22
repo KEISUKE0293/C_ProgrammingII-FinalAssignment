8215abe86eeceb7d7b0b542360116918296acd31
cgug1031 <cgug1031@mail4.doshisha.ac.jp>
2023-07-22 22:32:16 +0900

    汎用定数の定義（仮）

---
+++ 期末課題/consts.h
@@ -0,0 +1,17 @@
+#define BUFFSIZE 1024
+#define CHARBUFF 124
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+struct Coordinate {
+	int x;
+	int y;
+};
+
+struct Pixel {
+	bool isMined;
+	int mine_cnt;
+	Pixel* Up;
+	Pixel* Down;
+	Pixel* Left;
+	Pixel* Right;
+};
